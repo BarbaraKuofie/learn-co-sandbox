f1d7fbcfdc48cdd48293bc72ef1c1af03a5769f7
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-18 04:32:43 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,11 @@
+#This is a comment
+#perform a constant expression evaluation 
+3
+#assian constant 3 to bare-world varialbles trinagle_sides
+trinagle_sides = 3 
+#Lizo's lyrics from "heaven help me" is deep and soulful. here they are. 
+puts "Heaven help me\n"
+puts "If love aint dead I'ma kill it 'cause it's killing me"
+## this is the best part 
+puts "cold world, they'll be living in a fantasy"
+puts "Got me, it's the only thing i'll ever need"
